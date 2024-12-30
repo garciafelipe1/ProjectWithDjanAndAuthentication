5d12852cfaecda9aa74ff571fe53bd656aad989f
Felipe <feli2003garcia@gmail.com>
2024-12-30 19:36:17 -0300

    formulario creado, falta base de datos

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from tasks import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.helloword)
+    path('', views.helloword),
+    path('signup/',views.helloword)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def helloword(request):
-    return HttpResponse ('Hello word')
+    return render(request,'signup.html',{
+        'form': UserCreationForm
+    })
     
