6d7662f583ce64b8e77c2ddaa52469d96989b087
Felipe <feli2003garcia@gmail.com>
2025-01-02 10:00:02 -0300

    model: tasks

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -14,13 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from tasks import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name= 'home'),
-    path('signup/',views.signup, name= 'home'),
-    path('tasks/',views.tasks, name= 'tasks')
-    
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("signup/", views.signup, name="home"),
+    path("tasks/", views.tasks, name="tasks"),
+    path("logout/", views.signout, name="logout"),
+    path("signin/", views.signin, name="signin"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login,logout,authenticate
 from http.client import HTTPResponse
 
 
@@ -40,3 +40,24 @@ def signup(request):
 def tasks(request):
     return render(request, 'tasks.html')
 
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render (request, 'signin.html',{
+        'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request,username=request.POST['username'],password=request.POST['password'])
+        if user is None: 
+            return render (request, 'signin.html',{
+            'form': AuthenticationForm,
+            'error': 'username or password is incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
+    
