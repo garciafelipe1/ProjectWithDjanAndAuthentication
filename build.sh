89a656dbc37d69091e50751b4792cf6fbe45f2fd
Felipe <feli2003garcia@gmail.com>
2025-01-06 10:07:41 -0300

    hecho

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
-set -errexit
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
 
-# poetry install
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
 
-# pip install  -r requirements.txt
+# Convert static asset files
+python manage.py collectstatic --no-input
 
-python manage.py collectstatic --noinput
+# Apply any outstanding database migrations
 python manage.py migrate
-
-ch
